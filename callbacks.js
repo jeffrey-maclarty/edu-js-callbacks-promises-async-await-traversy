2c9896b2b70985c38195f297319671412143898e
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-03-29 09:02:48 -0400

    Complete

---
+++ callbacks.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const posts = [
+    {title: 'Post One', body: 'This is post one'},
+    {title: 'Post Two', body: 'This is post two'}
+];
+
+function getPosts() {
+
+    // setTimeout(() => {
+    //
+    // }, 2000)
+
+    setTimeout(function () {
+        let output = '';
+        posts.forEach(function (post) {
+            output += `<li>${post.title}</li>`
+        })
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+
+function createPost(post, callback) {
+    setTimeout(function () {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+// getPosts();
+
+createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
